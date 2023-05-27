dd5f7b419d500d7ef50491a670dd0b219af3e8a7
Rostislav Jadavan <rostislav@jadavan.com>
2023-05-27 22:07:52 +0200

    homepage, getting started and typescript updates

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import {DocsThemeConfig} from 'nextra-theme-docs'
 import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <Image src="/watchcat_logo.svg" width={200} height={40} alt="watchcatdocs"/>,
-  docsRepositoryBase: 'https://github.com/WatchCatHQ/watchcat-docs',
-  darkMode: false,
-  primaryHue: 245,
-  footer: {
-    text: 'WatchCat Docs',
-  },
-  useNextSeoProps() {
-    return {
-      titleTemplate: '%s – WatchCat Docs'
+    logo: <Image src="/watchcat_logo.svg" width={200} height={40} alt="watchcatdocs"/>,
+    docsRepositoryBase: 'https://github.com/WatchCatHQ/watchcat-docs',
+    darkMode: false,
+    primaryHue: 245,
+    footer: {
+        text: 'WatchCat Docs',
+        component: <>{process && process.env.NODE_ENV === 'production' &&
+            <script src="https://cdn.usefathom.com/script.js" data-site="ULEDDWGW" defer></script>}
+        </>
+    },
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s – WatchCat Docs'
+        }
     }
-  }
 }
 
 export default config
